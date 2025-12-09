386fee911a953431389e8d453a3cdcfb412a9b28
DevSonar <devsonar19@outlook.com>
2025-12-09 23:39:52 +0530

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -32,9 +32,58 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
   }
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login View'),),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter Your Email: ',
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter Your Password: ',
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              final userCredentials = await FirebaseAuth.instance
+                  .signInWithEmailAndPassword(
+                email: email,
+                password: password,
+              );
+              print(userCredentials);
+            },
+            child: const Text('Login'),
+          ),
+          TextButton(
+            onPressed: (){
+              Navigator.of(context).pushNamedAndRemoveUntil('/register/',
+                      (route)=> false
+              );
+            },child: const Text('Not Registered? Register Here!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
-}
