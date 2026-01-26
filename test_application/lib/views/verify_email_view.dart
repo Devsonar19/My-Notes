9e6c90b740cc8a6f6bb0643a026e7052883f65be
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-26 15:17:52 +0530

    improved verify email view, better ui and buttons, more clear instructions

--- test_application/lib/views/verify_email_view.dart
+++ test_application/lib/views/verify_email_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:test_application/constants/routes.dart';
 import 'package:test_application/services/auth/auth_service.dart';
 
@@ -17,32 +20,123 @@ class VerifyEmailView extends StatefulWidget {
 class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Email Verification View'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          const Text("Email Verification Link has been Delivered, Please open link to Verify Account"),
-          const Text("If you haven't, Please click Below Button"),
-          TextButton(onPressed: ()  {
-            context.read<AuthBloc>().add(
-                const AuthEventSendEmailVerification(),
-            );
-          }, child: const Text('Send email verification')
-          ),
-          TextButton(
-              onPressed: () async{
-                context.read<AuthBloc>().add(
-                    const AuthEventLogOut(),
-                );
-              },
-              child: const Text('Restart'),
-          )
-        ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
         ),
-      ),
+        child:
+          Stack(
+            children: [
+                Positioned.fill(
+                child: Image.asset(
+                    'assets/images/verify_email_view_background.jpg',
+                    fit: BoxFit.cover
+                  ),
+                ),
+                Positioned.fill(
+                child: Container(
+                color: Colors.black.withValues(alpha: 0.5),
+                  ),
+                ),
+              Scaffold(
+                resizeToAvoidBottomInset: true,
+                backgroundColor: Colors.transparent,
+              body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 350,
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 50),
+                          child: Text(
+                              "Email Verification Link has been Delivered, Please open link to Verify Account",
+                              style: GoogleFonts.ooohBaby(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                                color: Colors.white,
+                              ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          height: 80.0,
+                          width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              'If you have not received the email, click the button below to send it again',
+                              style: GoogleFonts.handlee(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.white
+                              )
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 80.0,
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              'If you have already verified your email, go back and Login',
+                              style: GoogleFonts.handlee(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.white
+                              )
+                          ),
+                        ),
+                      ),
+                    const SizedBox(height: 20.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: HexColor('FDEB9E'),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: ()  {
+                        context.read<AuthBloc>().add(
+                            const AuthEventSendEmailVerification(),
+                        );
+                      }, child: const Text('Send Again', style: TextStyle(fontSize: 20))
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: HexColor('F6F6F6'),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                          onPressed: () async{
+                            context.read<AuthBloc>().add(
+                                const AuthEventLogOut(),
+                            );
+                          },
+                          child: const Text('Back to Login', style: TextStyle(fontSize: 20)),
+                      ),
+                    )
+                  ],
+                  ),
+                ),
+              ),
+              ),
+            ],
+          ),
     );
   }
 }
